c9eeec57572630c9e9ae6cd94a7685741524cf83
arkopalbhattacharya <arkopal.bhattacharya@live.com>
2022-03-20 01:43:30 +0530

    Benette theme - main theme colors

---
+++ index.js
@@ -0,0 +1,58 @@
+const theme = {
+  "Brick Red": "c33c54",
+  "Indigo Dye": "254e70",
+  "Celeste": "aef3e7",
+  "Teal Blue": "37718e",
+  "Sky Blue Crayola": "8ee3ef",
+  extendedArray: {
+    [
+      {
+        "name": "Brick Red",
+        "hex": "c33c54",
+        "rgb": [195, 60, 84],
+        "cmyk": [0, 69, 57, 24],
+        "hsb": [349, 69, 76],
+        "hsl": [349, 53, 50],
+        "lab": [46, 55, 17]
+      },
+      {
+        "name": "Indigo Dye",
+        "hex": "254e70",
+        "rgb": [37, 78, 112],
+        "cmyk": [67, 30, 0, 56],
+        "hsb": [207, 67, 44],
+        "hsl": [207, 50, 29],
+        "lab": [32, -3, -24]
+      },
+      {
+        "name": "Celeste",
+        "hex": "aef3e7",
+        "rgb": [174, 243, 231],
+        "cmyk": [28, 0, 5, 5],
+        "hsb": [170, 28, 95],
+        "hsl": [170, 74, 82],
+        "lab": [91, -24, -1]
+      },
+      {
+        "name": "Teal Blue",
+        "hex": "37718e",
+        "rgb": [55, 113, 142],
+        "cmyk": [61, 20, 0, 44],
+        "hsb": [200, 61, 56],
+        "hsl": [200, 44, 39],
+        "lab": [45, -10, -22]
+      },
+      {
+        "name": "Sky Blue Crayola",
+        "hex": "8ee3ef",
+        "rgb": [142, 227, 239],
+        "cmyk": [41, 5, 0, 6],
+        "hsb": [187, 41, 94],
+        "hsl": [187, 75, 75],
+        "lab": [85, -23, -14]
+      }
+    ]
+  }
+};
+
+export default theme;
